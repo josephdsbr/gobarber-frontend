08de5d7ca7f23c810259c9eb556a5e78cee44379
josephdsbr <josephdsbr@gmail.com>
2019-11-13 23:37:20 -0300

    initial commit

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Form, Input} from '@rocketseat/unform';
+import logo from '~/assets/logo.svg';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Password" />
+        <button type="submit">Access</button>
+        <Link to="/register">Sign Up</Link>
+      </Form>
+    </>
+  );
+}
