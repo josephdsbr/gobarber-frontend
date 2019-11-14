5009e13930c76963494bf1bc58237a59acba8cdd
josephdsbr <josephdsbr@gmail.com>
2019-11-14 09:50:38 -0300

    configurando o redux e redux-saga e adicionando layout das telas

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {Form, Input} from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import {signInRequest} from '~/store/modules/auth/actions';
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insert a valid email')
+    .required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
 
 export default function SignIn() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+
+  function handleSubmit({email, password}) {
+    dispatch(signInRequest(email, password));
   }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="E-mail" />
         <Input name="password" type="password" placeholder="Password" />
         <button type="submit">Access</button>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+
 import {Form, Input} from '@rocketseat/unform';
+
+import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+  nome: Yup.string().required('Name is required'),
+  email: Yup.string()
+    .email('Insert a valid email')
+    .required('Email is required'),
+  password: Yup.string().min(6, 'Password has a minimum of 6 characters').required('Password is required'),
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -12,7 +21,7 @@ export default function SignUp() {
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Full name" />
         <Input name="email" type="email" placeholder="E-mail" />
         <Input name="password" type="password" placeholder="Password" />
